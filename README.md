39592c81ba95836f82659f96af2c50b9a657a3ec
Jackie Fletcher <jaxskriv@gmail.com>
2015-01-09 13:36:04 -0800

    include final edits and styling, update readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 This method checks for the frequency of a word within a phrase. No partial matches accounted for at this time. 
 
+Using Ruby 2.2.0
+Run $ install bundler then $ bundle to install gams
+
 Party on!
+Developed by Jackie Skrivseth Fletcher
+
+GPL v2
--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,33 +1,8 @@
-# class String
-#   define_method(:word_frequency) do |words|
-#     words = words.downcase()
-#     original_word = self.downcase()
-#     words_array = words.split(/[\s,'!.-_"?]/)
-#     count_array = []
-#
-#     if words_array.include?(original_word)
-#         words_array.each() do |word|
-#           if original_word == word
-#             count_array.push(1)
-#           end
-#         end
-#     else
-#       return "Your word does not appear in these words!"
-#     end
-#
-#     sum = 0
-#     count_array.each() do |number|
-#       sum = sum.+(number)
-#     end
-#     sum
-#   end
-# end
-
 class String
   define_method(:word_frequency) do |words|
     words = words.downcase()
     original_word = self.downcase()
-    words_array = words.split(/[\s,'!.-_"?]/)
+    words_array = words.split(/[\s,'!-._"?]/)
     count_array = []
 
     if words_array.include?(original_word)
