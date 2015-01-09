de0cdc78b45131c19c18027e32a1c3b8eb802ec5
Jackie Fletcher <jaxskriv@gmail.com>
2015-01-09 13:41:02 -0800

    add tweaks from final review

--- app.rb
+++ app.rb
@@ -8,13 +8,12 @@ get ('/form') do
 end
 
 get('/result') do
-
   @result = (params.fetch('original_word')).word_frequency(params.fetch('phrase_words'))
+  # Leaving the below in here because I would like to ask questions about it.
   # @times = if (params.fetch('original_word')).word_frequency(params.fetch('phrase_words')) == 1
   #             return "time"
   #           else
   #             return "times"
   #           end
-
   erb(:result)
 end
