f21917dcdcf8344151a440e0a24bde303f279787
Jackie Fletcher <jaxskriv@gmail.com>
2015-01-09 10:14:00 -0800

    first spec functions

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,5 +1,22 @@
 class String
-  define_method(:wordfrequency) do
-    #insert code here
+  define_method(:word_frequency) do |words|
+    words_array = words.split(" ")
+    count_array = []
+
+    if words_array.include?(self)
+        words_array.each() do |word|
+          if self == word
+            count_array.push(1)
+          end
+        end
+    else
+      returns "Your word does not appear in these words!"
+    end
+
+    sum = 0
+    count_array.each() do |number|
+      sum = sum.+(number)
+    end
+    sum
   end
 end
